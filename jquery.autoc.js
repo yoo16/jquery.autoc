9dacb81626fee5d7bd76a02bf74f63d492dbe9eb
yoo16 <yohei.yoshikawa@gmail.com>
2021-01-16 22:15:41 +0900

    bug fix

--- jquery.autoc.js
+++ jquery.autoc.js
@@ -1,6 +1,8 @@
 /**
- * @file This is jQuery auto toc script.
+ * @title jquery.autoc.js
+ * @file This is auto toc jQeury script.
  * @copyright Yohei Yoshikawa 2021
+ * @GitHub https://github.com/yoo16/jquery.autoc
  */
 const mokuji = (params) => {
     if (!params) params = {};
@@ -23,82 +25,95 @@ const mokuji = (params) => {
         tag: 'h2',
         class: ['h2']
     };
+    var a = {
+        class: {},
+        css: {},
+    }
     var ul = { class: ['list-group'] };
     var li = { class: ['list-group-item'] };
 
-    var htmlTag = (name) => {
-        return '<' + name + '>';
-    };
+    const htmlTag = (name) => { return '<' + name + '>'; };
+    const idSelector = (name) => { return '#' + name; };
 
     var loadParams = (params) => {
         if (params.start) start = params.start;
         if (params.end) end = params.end;
 
+        if (params.a) {
+            if (params.a.class) a.class = params.a.class;
+            if (params.a.css) a.css = params.a.css;
+        }
         if (params.base) {
             if (params.base.class) base.class = params.base.class;
         }
         if (params.ul) {
             if (params.ul.class) ul.class = params.ul.class;
         }
-    }
-
-    var renderTitleTag = (params) => {
         if (params.title) {
             if (params.title.label) title.label = params.title.label;
             if (params.title.tag) title.tag = tag.element(params.title.tag);
             if (params.title.class) title.class = params.title.class;
         }
-
-        if (title.tag) title.tag = title.tag;
-        var titleTag = htmlTag(title.tag);
-        var titleElement = $(titleTag).addClass(title.class).text(title.label);
-        $(baseElement).prepend(titleElement);
     }
 
-    var renderToc = () => {
-        var baseElement = $(htmlTag(base.tag));
-        var ulElement = $(htmlTag('ul'));
-        var i = 0;
+    var baseElement = $(htmlTag(base.tag));
+    var render = (params) => {
+        var renderTitleTag = (params) => {
+            if (title.tag) title.tag = title.tag;
+            var titleTag = htmlTag(title.tag);
+            var titleElement = $(titleTag).addClass(title.class).text(title.label);
+            $(baseElement).prepend(titleElement);
+        }
 
-        //ul element
-        ulElement.addClass(ul.class);
+        var renderToc = () => {
+            var ulElement = $(htmlTag('ul'));
+            var i = 0;
 
-        //base element
-        $(baseElement).append(ulElement);
-        baseElement.addClass(base.class);
+            //ul element
+            ulElement.addClass(ul.class);
 
-        //target
-        if (params.id) targetId = params.id;
-        $(targetId).append(baseElement);
+            //base element
+            $(baseElement).append(ulElement);
+            baseElement.addClass(base.class);
 
-        var reg = new RegExp('h([' + start + '-' + end + '])', 'i');
-        $('*').each(function () {
-            var hMatch = $(this).get(0).tagName.match(reg);
-            if (hMatch) {
-                var hLevel = 2;
-                if (hMatch[1]) hLevel = hMatch[1];
-                if ($(this).attr('class').match(reg)) {
-                    i++;
-                    var idName = 'header_' + i;
-                    $(this).prop('id', idName);
+            //target
+            if (params.id) targetId = idSelector(params.id);
+            $(targetId).append(baseElement);
 
-                    var aElement = $(htmlTag('a')).attr('href', '#' + idName).html($(this).html());
-                    var liElement = $(htmlTag('li')).addClass(li.class);
-                    if (params.li) {
-                        liElement.removeClass();
-                        if (params.li.class) liElement.addClass(params.li.class);
+            var reg = new RegExp('h([' + start + '-' + end + '])', 'i');
+            $('*').each(function () {
+                var hMatch = $(this).get(0).tagName.match(reg);
+                if (hMatch) {
+                    var hLevel = 2;
+                    if (hMatch[1]) hLevel = hMatch[1];
+                    if ($(this).attr('class').match(reg)) {
+                        i++;
+                        var idName = 'header_' + i;
+                        $(this).prop('id', idName);
+
+                        var aElement = $(htmlTag('a')).attr('href', '#' + idName).html($(this).html());
+                        console.log(a.class);
+                        aElement.addClass(a.class).css(a.css);
+                        var liElement = $(htmlTag('li')).addClass(li.class);
+                        if (params.li) {
+                            liElement.removeClass();
+                            if (params.li.class) liElement.addClass(params.li.class);
+                        }
+                        if (params.level && (liLevel = params.level[hLevel])) {
+                            console.log(liLevel.class);
+                            if (liLevel.class) liElement.addClass(liLevel.class);
+                        }
+                        liElement.css(level[hLevel].css).append(aElement);
+                        $(ulElement).append(liElement);
                     }
-                    if (params.level) {
-                        if (params.level[hLevel]) liElement.addClass(params.level[hLevel]);
-                    }
-                    liElement.css(level[hLevel].css).append(aElement);
-                    $(ulElement).append(liElement);
                 }
-            }
-        });
+            });
+        }
+
+        renderToc(params);
+        renderTitleTag(params);
     }
 
     loadParams(params);
-    renderToc(params);
-    renderTitleTag(params);
+    render(params);
 }
